fca5e340b794d88cb187473f5db76fe2605ae9d8
sttv <sttv-pc@mail.ru>
2017-01-12 16:40:09 +0300

    DEV tests

--- tests/Test/DITest.php
+++ tests/Test/DITest.php
@@ -2,6 +2,7 @@
 
 namespace Test;
 
+use Deimos\DI\Group;
 use DeimosTest\TestSetUp;
 
 class DITest extends TestSetUp
@@ -9,7 +10,31 @@ class DITest extends TestSetUp
 
     public function testTest()
     {
-        $this->assertEquals(1, 1);
+        $this->assertEquals(
+            4,
+            $this->di->random()
+        );
+        $this->assertEquals(
+            $this->di->get('math.getRandom.getRandom'),
+            $this->di->random()
+        );
+        $this->assertEquals(
+            $this->di->math()->getRandom()->getRandom(),
+            $this->di->random()
+        );
+        $this->assertEquals(
+            $this->di->math()->get('getRandom.getRandom'),
+            $this->di->random()
+        );
+        $this->assertEquals(
+            $this->di->math()->call('getRandom.getRandom', []),
+            $this->di->random()
+        );
+var_dump( $this->di->pow());die;
+        $this->assertEquals(
+            81,
+            $this->di->pow()
+        );
     }
 
 }
--- tests/src/DI.php
+++ tests/src/DI.php
@@ -10,7 +10,29 @@ class DI extends \Deimos\DI\DI
      */
     protected function configure()
     {
-        $this->value('hello', 'привет');
+
+        $this->value('two', 2);
+        $this->value('nine', 9);
+
+        $this->group('math', function ()
+        {
+            $this->instance('getRandom', Get4::class, []);
+
+            $this->group('pow', function ()
+            {
+                $this->instance('mathClass', Math::class, []);
+            });
+        });
+
+        $this->addBuildCallback('random', function ()
+        {
+            return $this->call('math.getRandom.getRandom', []);
+        });
+
+        $this->callback('pow', function ()
+        {
+            return $this->call('math.pow.mathClass.pow', ['@nine', '@two']);
+        });
     }
 
 }
