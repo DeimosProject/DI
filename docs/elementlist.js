d2ce305e3ed79d8b7aec6098a0002c065fc62a1e
rez1dent3 <maksim.babichev95@gmail.com>
2017-01-13 14:25:12 +0300

    add docs

---
+++ docs/elementlist.js
@@ -0,0 +1,3 @@
+
+var ApiGen = ApiGen || {};
+ApiGen.elements = [["c","Deimos\\DI\\Container"],["c","Deimos\\DI\\DI"],["c","Deimos\\DI\\Group"],["c","Deimos\\DI\\Reflection"],["c","Exception"],["c","InvalidArgumentException"],["c","LogicException"],["c","Throwable"]];
